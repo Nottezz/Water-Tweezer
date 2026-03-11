cd5088327f4330db8c61505f07fcc13903636726
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-03-11 10:43:28 +0300

    add project config

---
+++ water_bot/config.py
@@ -0,0 +1,82 @@
+import logging
+from pathlib import Path
+from typing import Literal
+
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+BASE_DIR = Path(__file__).resolve().parent
+ROOT_DIR = BASE_DIR.parent
+
+
+class LoggingConfig(BaseModel):
+    log_format: str = (
+        "[-] %(asctime)s [%(levelname)s] %(module)s-%(lineno)d - %(message)s"
+    )
+    worker_log_format: str = (
+        "[-] %(asctime)s [%(levelname)s] [%(processName)s] %(module)s-%(lineno)d - %(message)s"
+    )
+    log_level_name: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "WARNING"
+    log_date_format: str = "%Y-%m-%d %H:%M:%S"
+
+    @property
+    def log_level(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level_name]
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_file=(
+            ROOT_DIR / ".env.template",
+            ROOT_DIR / ".env",
+        ),
+        env_prefix="WTBOT__",
+        env_nested_delimiter="__",
+        yaml_file=(
+            ROOT_DIR / "config.default.yaml",
+            ROOT_DIR / "config.local.yaml",
+        ),
+        yaml_config_section="wtbot",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Define the sources and their order for loading the settings values.
+
+        Args:
+            settings_cls: The Settings class.
+            init_settings: The `InitSettingsSource` instance.
+            env_settings: The `EnvSettingsSource` instance.
+            dotenv_settings: The `DotEnvSettingsSource` instance.
+            file_secret_settings: The `SecretsSettingsSource` instance.
+
+        Returns:
+            A tuple containing the sources and their order for loading the settings values.
+        """
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            YamlConfigSettingsSource(settings_cls),
+        )
+
+    logging: LoggingConfig = LoggingConfig()
+    telegram_bot_token: str
+
+
+settings = Settings()
