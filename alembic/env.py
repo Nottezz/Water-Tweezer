165dd1fbcfcf5462ff509363791dad019a04027c
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-03-16 17:03:00 +0300

    init alembic

---
+++ alembic/env.py
@@ -0,0 +1,62 @@
+import asyncio
+
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import AsyncConnection, create_async_engine
+
+from alembic import context
+from water_bot.config import settings
+from water_bot.models import Base, UserSettings
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = settings.database.database_url_asyncpg
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    connectable = create_async_engine(
+        settings.database.database_url_asyncpg,
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+def do_run_migrations(connection: AsyncConnection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
