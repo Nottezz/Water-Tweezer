3d122307815619d0b6630078b63f59693dcdd7d9
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-04-06 13:28:22 +0300

    add callback handlers

--- water_bot/routers/__init__.py
+++ water_bot/routers/__init__.py
@@ -2,7 +2,9 @@ __all__ = ("router",)
 
 from aiogram import Router
 
+from .callbacks import router as callbacks_router
 from .commands import router as commands_router
 
 router = Router(name=__name__)
 router.include_router(commands_router)
+router.include_router(callbacks_router)
